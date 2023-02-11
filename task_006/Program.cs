c23d48d107f21af66184b718fe1e4b7f6b27f2ae
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-11 19:37:05 +0300

    add task 006

---
+++ task_006/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая на вход принимает число и выдаёт, 
+//  является ли число чётным (делится ли оно на два без остатка).
+//   4 -> да
+//  -3 -> нет
+//   7 -> нет
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!); Math.Abs(-number);
+
+
+if(number % 2 == 0)
+{   
+Console.WriteLine("YES");
+}
+else
+{ 
+Console.WriteLine("NO");
+}
