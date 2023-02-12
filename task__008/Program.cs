bc5d265c5924db9835f15c56ae64052d465df7bf
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-12 14:10:28 +0300

    add task 008

---
+++ task__008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+int res = 2;
+
+while (res <= number)
+{
+    if (res % 2 == 0)
+    {
+        Console.Write(res + (" "));
+    }
+    res++;
+}
