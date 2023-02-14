41ba900cae0ee75d0032b4bbcb7af165b1fa547d
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-14 09:18:55 +0300

    добавил решение без метода

--- task__008/Program.cs
+++ task__008/Program.cs
@@ -13,42 +13,57 @@
 // {
 //     if (res % 2 == 0)
 //     {
-//         Console.Write(res + (" "));
+//         Console.Write(res + (", "));
 //     }
 //     res++;
 // }
 
 // Вывод чисел через запятую
-Console.Write("Enter a number: ");
-int number = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter a number: ");
+// int number = int.Parse(Console.ReadLine()!);
 
-void FillArray(int[] collection)
-{
-    number = collection.Length;
-    collection[0] = 1;
-    int index = 1;
-    while (index < number)
-    {
+// void FillArray(int[] collection)
+// {
+//     number = collection.Length;
+//     collection[0] = 1;
+//     int index = 1;
+//     while (index < number)
+//     {
 
-        collection[index] = index + 1;
-        index++;
-    }
-}
+//         collection[index] = index + 1;
+//         index++;
+//     }
+// }
 
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
 
-        position++;
+//         position++;
 
-    }
-}
-int[] array = new int[number];
-FillArray(array);
-PrintArray(array);
-//Console.Write(string.Join(", ",array.Select(x =>x.ToString())));
-int[] evenArr = array.Where(x => x % 2 == 0).ToArray();
-Console.WriteLine(string.Join(", ", evenArr.Select(x => x.ToString())));
+//     }
+// }
+// int[] array = new int[number];
+// FillArray(array);
+// PrintArray(array);
+// //Console.Write(string.Join(", ",array.Select(x =>x.ToString())));
+// int[] evenArr = array.Where(x => x % 2 == 0).ToArray();
+// Console.WriteLine(string.Join(", ", evenArr.Select(x => x.ToString())));
+
+
+
+//Решение без метода
+
+// Console.Write("Enter a number: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int i = 2;
+// while (i<=n) 
+// {
+// Console.Write(i);
+// if (i < n-1) 
+// { Console.Write(", "); }
+// i +=2;
+// }
